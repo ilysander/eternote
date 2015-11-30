67dcbb4bed4cb8f0277711287b515f33828e665c
Renzo Delgado <renzodelgadoguerra@gmail.com>
2015-11-29 23:36:34 -0500

    mejoras

--- frontend/components/note/note.js
+++ frontend/components/note/note.js
@@ -1,3 +1,26 @@
+$.fn.getCursorPosition = function(){
+	if(this.lengh == 0) return -1;
+	return $(this).getSelectionStart();
+}
+
+$.fn.getSelectionStart = function(){
+	if(this.lengh == 0) return -1;
+	input = this[0];
+
+	var pos = input.value.length;
+
+	if (input.createTextRange) {
+		var r = document.selection.createRange().duplicate();
+		r.moveEnd('character', input.value.length);
+		if (r.text == '') 
+		pos = input.value.length;
+		pos = input.value.lastIndexOf(r.text);
+	} else if(typeof(input.selectionStart)!="undefined")
+	pos = input.selectionStart;
+
+	return pos;
+}
+
 $(function(){
 
 	var $content = $('#content');
@@ -6,9 +29,13 @@ $(function(){
 
 	socket.emit('join', { textId: $id.val() });
 
-	$content.keyup(function(event) {
+	$content.keyup(function(e) {
 		var text = { id: $id.val(), content: $content.val() }
 		socket.emit('change', { text: text });
+	});
+
+	$content.click(function(e) {
+		console.log($content.getCursorPosition());
 	});
 
 	socket.on('change', function(message) {
